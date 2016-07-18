71295473de735360130946bcd973501eb3b1e217
Mark Johnson <markjohnson303@gmail.com>
2016-07-18 16:27:10 -0600

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-require './models/artist'
+require './models/event'
 
-Artist.create({
-		name: 'Luciano Pavarotti',
-		photo_url: "http://artcreationforever.com/images/luciano-pavarotti/luciano-pavarotti-03.jpg", 
-		nationality: 'Italiano',
-		instrument: "Voice", 
-		home_address: "1 Strada Roma"
+Event.create({
+		title: 'Generic Event',
+		image_url: "https://static.pexels.com/photos/5156/people-eiffel-tower-lights-night-large.jpg", 
+		Location: 'Denver',
+		description: 'this is my description'
+		link: "https://www.pexels.com/photo/people-eiffel-tower-lights-night-5156/", 
+		time_sensitive: true
 	})
