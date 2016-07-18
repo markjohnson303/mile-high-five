bbd464de7d5e11f77ab40030c1adfa837b9b1017
Mark Johnson <markjohnson303@gmail.com>
2016-07-18 16:29:55 -0600

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Event.create({
 		title: 'Generic Event',
 		image_url: "https://static.pexels.com/photos/5156/people-eiffel-tower-lights-night-large.jpg", 
 		Location: 'Denver',
-		description: 'this is my description'
+		description: 'this is my description',
 		link: "https://www.pexels.com/photo/people-eiffel-tower-lights-night-5156/", 
 		time_sensitive: true
 	})
